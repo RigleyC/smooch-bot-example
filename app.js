35ff0c447ba5a49d02216a2aa13af7d64f28a022
RigleyC <rigley2010@hotmail.com>
2017-01-16 22:49:51 -0200

    testing :/

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
+/*
 app.get('/', function(req, res) {
+    res.render('index', {
+        appToken: process.env.SMOOCH_APP_TOKEN
+    });
+});
+
+module.exports = app;
+*/
+
+app.get('/', function(req, res) {
+
+if (req.query['hub.verify_token'] === 'hi') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+//--------------------------------------------
     res.render('index', {
         appToken: process.env.SMOOCH_APP_TOKEN
     });
